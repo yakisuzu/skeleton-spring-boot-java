7931d52c9f537b86b5ffc1c7a5ba98adb0595e65
yakisuzu <yakisuzu@gmail.com>
2019-08-21 00:46:24 +0900

    prettier+eslint+@typescript-eslint

--- ops/cdk/bin/sample-app.ts
+++ ops/cdk/bin/sample-app.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-import cdk = require('@aws-cdk/core');
-import { SampleAppStack } from '../lib/sample-app-stack';
+import cdk = require('@aws-cdk/core')
+import { SampleAppStack } from '../lib/sample-app-stack'
 
-const app = new cdk.App();
-new SampleAppStack(app, 'SampleAppStack');
+const app = new cdk.App()
+new SampleAppStack(app, 'SampleAppStack')
